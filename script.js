1abfa922026c4c3247157020112dd6b94adc9640
Abhi5661 <114367329+Abhi5661@users.noreply.github.com>
2023-01-31 16:55:18 +0530

    JS Classroom assignment-1

---
+++ script.js
@@ -0,0 +1,14 @@
+function theme1() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+
+function theme2() {
+  document.body.style.backgroundColor = "grey";
+  document.body.style.color = "black";
+}
+
+function theme3() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "grey";
+}
